ce812fe9954c3ed8c7ebf224b89cc6c76f5eceb0
MotoakiFukuda <motoaki.fukuda@bravi.co.jp>
2017-07-24 06:46:36 +0000

    user login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>ログイン画面です</h1>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            
+            {!! Form::open(['route' => 'login.post']) !!}
+                <div class="form-group">
+                    {!! Form::label('email', 'E-mailアドレスを入力') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('password', 'パスワードを入力') !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('ログインする', ['class' => 'btn btn-primary btn-block']) !!}
+            {!! Form::close() !!}
+            
+            <p>ユーザ登録はお済ですか？ {!! link_to_route('signup.get', 'ユーザ登録はこちら') !!}</p>
+        </div>
+    </div>
+@endsection
--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,8 +12,18 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_route('signup.get', '新規登録はこちら') !!}</li>
-                    <li><a href="#">ログインはこちら</a></li>
+                    @if (Auth::check())
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li role="separator" class="divider"></li>
+                                <li>{!! link_to_route('logout.get', 'ログアウトする') !!}</li>
+                            </ul>
+                        </li>
+                    @else
+                        <li>{!! link_to_route('signup.get', '新規登録はこちら') !!}</li>
+                        <li>{!! link_to_route('login.get', 'ログインはこちら') !!}</a></li>
+                    @endif
                 </ul>
             </div>
         </div>
