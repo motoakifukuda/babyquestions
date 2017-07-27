8701c77cabe64f71ab103c2663150b4989121a19
MotoakiFukuda <motoaki.fukuda@bravi.co.jp>
2017-07-27 06:57:56 +0000

    20170727-6

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -18,7 +18,7 @@
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::check())
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ログインユーザ名：{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
                                 <li role="separator" class="divider"></li>
                                 <li>{!! link_to_route('logout.get', 'ログアウトする') !!}</li>
